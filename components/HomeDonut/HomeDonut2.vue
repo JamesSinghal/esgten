1482ec89678928cca58b68a7d974d193ef7e500f
VA_24 <60634457+VA-24@users.noreply.github.com>
2021-10-03 03:06:43 -0500

    Add files via upload
    if these dont pass build tests, take them out of this dir

---
+++ components/HomeDonut/HomeDonut2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="donut">
+    <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+  </div>
+</template>
+
+<script>
+import DonutChart from "./DonutChart";
+
+export default {
+    name: "HomeDonut2",
+  components: {
+    DonutChart
+  },
+  data() {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 20
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 10,
+        labels: ["BA","HD","MRO","UNH","WST","XOM"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#11633f", "#10c274", "#10c273", "#10c272", "#10c271", "#10c270"],
+            data: [16.9, 23.1, 11.6, 10.3, 17.9, 20.2]
+          }
+        ]
+      }
+    };
+  }
+}
+</script>
