e2dd43c7eb8569f396581c4029f2bf1a82bcb5c4
iiisong <isaacsong03@gmail.com>
2021-10-03 07:48:15 -0500

    small fixs

---
+++ components/treeMap.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chartjs-chart-treemap@0.2.3"></script>
+
+        <div class="canvas-holder">
+            <canvas id="chart-area" width="800" height="400"></canvas>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: treeMap,
+}
+
+
+</script>
--- pages/insights.vue
+++ pages/insights.vue
@@ -3,24 +3,77 @@
     <link rel="stylesheet" href="styles.css">
     <backGrad></backGrad>
     <sideBar></sideBar>
-    <div class="grid2x2">
-      <div class="box box1"><div>one</div></div>
-      <div class="box box2"><div>two</div></div>
-      <div class="box box3"><div>three</div></div>
-      <div class="box box4"><div>four</div></div>
-  </div>
+    <div id="app">
+      <div id="insight-flex" class="grid2x2">
+        <div id="ports-panel" class="panel box box1">
+          <div class="ports-port">
+            <div style="background-color:lightgreen" class="port-score-box">9.0</div>
+            <div class="port-info">DFS</div>
+          </div>
+          <div class="ports-port">
+            <div style="background-color:yellow" class="port-score-box">7.7</div>
+            <div class="port-info">y</div>
+          </div>
+          <div class="ports-port">
+            <div style="background-color:orange" class="port-score-box">6.3</div>
+            <div class="port-info">y</div>
+          </div>
+          <div class="ports-port">
+            <div style="background-color:red" class="port-score-box">5.7</div>
+            <div class="port-info">y</div>
+          </div>
+        </div>
+        <div id="stock-panel" class="panel box box2">
+          <div class="stock-port">
+            <div style="background-color:lightgreen" class="stock-score-box">9.0</div>
+            <div class="stock-info">DFS</div>
+          </div>
+          <div class="stock-port">
+            <div style="background-color:yellow" class="stock-score-box">7.7</div>
+            <div class="stock-info">y</div>
+          </div>
+          <div class="stock-port">
+            <div style="background-color:orange" class="stock-score-box">6.3</div>
+            <div class="stock-info">y</div>
+          </div>
+          <div class="stock-port">
+            <div style="background-color:red" class="stock-score-box">5.7</div>
+            <div class="stock-info">y</div>
+          </div>
+          <div class="stock-port">
+            <div style="background-color:red" class="stock-score-box">5.7</div>
+            <div class="stock-info">y</div>
+          </div>
+        </div>
+        <div class="panel box box3"></div>
+        <div id="pies-panel" class="panel box box4"><div>
+          <HomeDonut1></HomeDonut1>
+          <HomeDonut2></HomeDonut2>
+          <HomeDonut3></HomeDonut3>
+          <HomeDonut4></HomeDonut4>
+          </div></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import sideBar from "../components/sideBar.vue"
   import backGrad from "../components/backGrad.vue"
+  import HomeDonut1 from "../components/HomeDonut/HomeDonut1.vue"
+  import HomeDonut2 from '../components/HomeDonut/HomeDonut2.vue'
+  import HomeDonut3 from "../components/HomeDonut/HomeDonut3.vue"
+  import HomeDonut4 from '../components/HomeDonut/HomeDonut4.vue'
 
   export default {
     name: 'App',
     components: {
       sideBar,
       backGrad,
+      HomeDonut1,
+      HomeDonut2,
+      HomeDonut3,
+      HomeDonut4
     },
   }
 </script>
@@ -31,9 +84,56 @@
 @tailwind components;
 @tailwind utilities;
 
-#ports-list {
-  padding-top: 1vh;
-  padding-bottom: 1vh;
+.stock-score-box {
+  width: 3vw;
+  text-align: center;
+}
+
+.stock-port {
+  height: 6.4vh;
+  border-bottom-width: 2px;
+}
+
+.stock-info {
+   padding-top: 1.5vh;
+}
+
+
+.ports-port {
+  height: 8vh;
+  border-bottom-width: 2px;
+}
+
+.ports-port > div {
+  padding-top: 1.5vh;
+}
+
+.port-score-box {
+  width: 3vw;
+  text-align: center;
+}
+
+.port-info {
+  width: 17vw;
+  padding-left: 1.5vw;
+}
+
+#ports-panel > hr {
+  height: 1vh;
+}
+
+#insight-flex {
+  width:88vw;
+  padding:2vw;
+}
+
+#ports-panel {
+  width:20vw;
+  height:32 vh;
+}
+
+#pies-panel {
+  height:20vh;
 }
 
 #ports-summ {
@@ -48,25 +148,23 @@
   flex-direction: row;
 }
 
-.grid2x2 > div {
+.panel {
   display: flex; 
   flex-basis: calc(50% - 40px);  
-  justify-content: center;
   flex-direction: column;
+  border-radius: 5px;
+  background: #ffffff;
+  box-shadow: 0px 0px 7px -3px #b9b9b9;
 }
 
 .grid2x2 > div > div {
   display: flex;
-  justify-content: center;
   flex-direction: row;
-    border-radius: 5px;
-  background: #ffffff;
-  box-shadow: 4px 0px 7px -3px #b9b9b9;
 }
 
 .box { margin: 2vh; }
 
-#panel {
+.panel {
   margin: 3vh;
   border-radius: 5px;
   background: #ffffff;
@@ -94,7 +192,7 @@
 }
 
 #app {
-  width: 100vw;
+  width: 88vw;
   margin-left: 12vw;
 }
 </style>
